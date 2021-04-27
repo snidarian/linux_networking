7463145b00a74ee4dec54cf91f27e12857d0fc87
snidarian <cephalopod31956@gmail.com>
2021-04-27 08:52:45 -0400

    traceroute doc-file

---
+++ traceroute.sh
@@ -0,0 +1,10 @@
+# Traceroute command traces the route from your local machine to an ip address.
+# it lists all the servers, dns servers, etc that a request from your local machine has
+# to go through to reach a foreign machine
+
+
+# for example you can use it like this:
+traceroute google.com
+
+# or like this:
+traceroute microsoft.com
