97cf58af549c74d79c84abbd5d1ba62c307035d3
snidarian <cephalopod31956@gmail.com>
2021-04-28 17:28:31 -0400

    arp file

---
+++ arp.sh
@@ -0,0 +1,33 @@
+# ARP stands for Address Resolution Protocol
+# The arp command resolves a device's ip address on a local network into its mac address
+
+
+# The arp cache is where out computer saves the associations it's learned about a network
+
+# using the command without arguments displays the arp cache.
+# no flag or -e displays it in linux style, -a displays it in bsd style
+sudo arp -e
+
+
+# to scan all devices on the network you can use a command called arp-scan with --localnet
+sudo arp-scan --localnet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
