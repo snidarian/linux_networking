375f3eaac341472e4ddac1742c74308b51cbf759
snidarian <cephalopod31956@gmail.com>
2021-05-02 19:06:53 +0000

    sftp docfile

---
+++ sftb.sh
@@ -0,0 +1,47 @@
+
+
+# sftp command is use similar to how the ssh command is used.
+
+# sftp username@<hostname or publicip>
+
+
+
+# you'll get a sftp prompt
+# <sftp>
+
+
+
+# commands to remember from within the prompt
+
+
+# lls
+# list local files
+
+# ls
+# list remove files
+
+
+# 'get'
+#  gets file from remote host and put it in local host
+
+# 'put'
+# puts a file from local host into the remote host directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
