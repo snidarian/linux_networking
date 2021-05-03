5081ab7253156a29930fdb7911afe34e91074634
snidarian <cephalopod31956@gmail.com>
2021-05-03 14:44:19 -0400

    netdiscover command

---
+++ netdiscover.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+
+# net discover is a active/passive network scanning tool
+# its best used for its passive scanning ability
+
+# Most basic use of the commmand:
+sudo netdiscover
+# this will passively record ARP requests by other devices in the network and aggregate them into a log
+# This log will display to the terminal
+
+
+
+
+
+
+
+
